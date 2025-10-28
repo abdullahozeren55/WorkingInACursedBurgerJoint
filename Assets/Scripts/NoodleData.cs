c6c10282a95c89f5f3537e69867e53435692040d
Abdullah Ozeren <ozeren55abdullah@gmail.com>
2025-10-28 15:28:16 +0300

    Noodle

--- Assets/Scripts/NoodleData.cs
+++ Assets/Scripts/NoodleData.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewNoodleData", menuName = "Data/Noodle")]
 public class NoodleData : ScriptableObject
 {
-    public bool isUseable = false;
+    public bool isUseable = true;
     public PlayerManager.HandGrabTypes handGrabType;
     [Space]
     public float timeToPutOnHologram = 0.3f;
+    [Space]
     public Vector3 grabPositionOffset;
     public Vector3 grabRotationOffset;
-
+    [Space]
+    public Vector3 grabLocalPositionOffset;
+    public Vector3 grabLocalRotationOffset;
+    [Space]
+    public Vector3 usePositionOffset;
+    public Vector3 useRotationOffset;
+    [Space]
+    public Vector3 useLocalPositionOffset;
+    public Vector3 useLocalRotationOffset;
+    [Space]
+    public float timeToUse = 0.3f;
+    [Space]
     public AudioClip[] audioClips;
 }
